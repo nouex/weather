18f15ffaa99cd436d2a93e413e82a769aafabe29
Reece Hudson <reecehudson10@gmail.com>
2017-09-12 15:26:43 -0500

    test: <DataBlockHeader />

--- src/components/DataBlockHeader.js
+++ src/components/DataBlockHeader.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from "prop-types"
+import iconToUrl from "../utils/iconToUrl"
 
 const DataBlockHeader = ({icon, summary}) => (
   <div>
-      <img src={getIcon(icon)} />
-      <p>summary</p>
+      <img src={iconToUrl(icon)} />
+      <p>{summary}</p>
   </div>
 )
+
+DataBlockHeader.propTypes = {
+  icon: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired
+}
 
 export default DataBlockHeader
---
+++ src/components/DataBlockHeader.test.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { shallow  } from 'enzyme';
+import DataBlockHeader from "./DataBlockHeader"
+
+describe('<DataBlockHeader />', function () {
+  it("renders correctly", function () {
+    const props = {
+      summary: "Rain for the hour.",
+      icon: "sunny"
+    }
+    const wrapper = shallow(<DataBlockHeader {...props}/>)
+
+    expect(wrapper.getNode()).toEqual(
+      <div>
+          <img src="./icons/sunny.png" />
+          <p>{props.summary}</p>
+      </div>
+    )
+  })
+});
