3c5dfa49e4c671fa3e965488a9f72357a1bdf5d2
Reece Hudson <reecehudson10@gmail.com>
2017-09-12 15:19:37 -0500

    test: <Header />

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,27 @@ import React from 'react';
 import cities from "cities"
 import iconToUrl from "../utils/iconToUrl"
 import PropTypes from 'prop-types'
+import moment from "moment-timezone"
 
-const Header = ({ latitude, longitude, icon, time, timezone, temperature }) => (
-  <div>
-    <img src={iconToUrl(icon)} />
-    <div>{temperature}°F</div>
+const Header = ({ latitude, longitude, icon, time, timezone, temperature }) => {
+  let city = cities.gps_lookup(latitude, longitude)
+  let location = `${city.city}, ${city.state_abbr}`
+  
+  return (
     <div>
-      <div>{cities.gps_lookup(latitude, longitude)}</div>
-      <div><date>{/* TODO */}</date></div>
+      <img src={iconToUrl(icon)} />
+      <div>{temperature}°F</div>
+      <div>
+        <div>{location}</div>
+        <div><date>{ moment.tz(time, timezone).format('h:mma z') }</date></div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
-Header.PropTypes = {
+Header.propTypes = {
   latitude: PropTypes.string.isRequired,
-  longitued: PropTypes.string.isRequired,
+  longitude: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
   time:  PropTypes.number.isRequired,
   timezone: PropTypes.string.isRequired,
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,8 +1,28 @@
-import { shallow, mount, render } from 'enzyme';
+import React from "react"
+import { shallow  } from 'enzyme';
 import Header from "./Header"
 
 describe('<Header />', function () {
-  it("passes????", function () {
+  it("renders icon, temp, location, and date", function () {
+    const props = {
+      latitude: "47.20296790272209",
+      longitude: "-123.41670367098749",
+      icon: "rain",
+      time: 1505247126663,
+      timezone: "America/Los_Angeles",
+      temperature: 48.71
+    }
+    const wrapper = shallow(<Header {...props}/>)
 
+    expect(wrapper.getNode()).toEqual(
+      <div>
+        <img src="./icons/rain.png" />
+        <div>{props.temperature}°F</div>
+        <div>
+          <div>Matlock, WA</div>
+          <div><date>{"1:12pm PDT"}</date></div>
+        </div>
+      </div>
+    )
   })
 });
