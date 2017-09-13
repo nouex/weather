1cc1380a46b74f6bb936949d6fd27ac0d9a21f87
Reece Hudson <reecehudson10@gmail.com>
2017-09-12 20:13:57 -0500

    containers: add <Header />

---
+++ src/containers/Header.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux"
+import React from "react"
+import Header from "../components/Header"
+
+function mapStateToProps(state) {
+  const data = state.data
+  return {
+    latitude: data.latitude,
+    longitude: data.longitude,
+    icon: data.currently.icon,
+    time: data.currently.time,
+    timezone: data.timezone,
+    temperature: data.currently.temperature
+  }
+}
+
+const _Header = connect(mapStateToProps)(Header)
+
+exports default _Header
