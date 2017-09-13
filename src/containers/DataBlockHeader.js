7edc510f8eb5a5b2b80644237348055371a7e1a7
Reece Hudson <reecehudson10@gmail.com>
2017-09-12 21:12:32 -0500

    containers: add <DataBlockHeader />

---
+++ src/containers/DataBlockHeader.js
@@ -0,0 +1,16 @@
+import { connect } from "react-redux"
+import React from "react"
+import DataBlockHeader from "../components/DataBlockHeader"
+
+function mapStateToProps(state) {
+  const curr = state.data.currently
+
+  return {
+    icon: curr.icon,
+    summary: curr.summary
+  }
+}
+
+const _DataBlockHeader = connect(mapStateToProps)(DataBlockHeader)
+
+exports default _DataBlockHeader
