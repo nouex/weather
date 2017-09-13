47abd42adcb2855e183aac8bc024b35da985c1da
Reece Hudson <reecehudson10@gmail.com>
2017-09-13 14:57:59 -0500

    finalize entry pt, index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import thunk from "redux-thunk"
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import reducer from "./reducer"
 
-ReactDOM.render(<_App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <_App />
+  </Provider>,
+  document.getElementById('root'));
