7437e5f934e8230cd2c7ab6a2e6b543ca90c1d1c
Reece Hudson <reecehudson10@gmail.com>
2017-09-19 12:46:38 -0500

    user server-static

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 // TODO morgan
 const app = require("express")(),
       morgan = require("morgan"),
+      serveStatic = require('serve-static')
       https = require("https"),
       fs = require("fs")
 
@@ -12,7 +13,7 @@ const secretKey =
   fs.readFileSync("./dark_sky_key.txt", { encoding: "utf8"}).trim()
 
 app.use(morgan("tiny"))
-
+app.use(serveStatic('build', {'index': ['index.html', 'index.htm']}))
 app.get('/weather/:lat/:long', function(req, res){
   const darkSkyEndpt = `https://api.darksky.net/forecast/${secretKey}/${req.params.lat},${req.params.long}`
 
